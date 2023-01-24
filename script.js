c69e1fd871b160f3366e3c04b25c67d5bb9b919d
diego <mauro3333@gmail.com>
2023-01-23 22:45:16 -0500

    final code

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+
+function switchOn(room) {
+    document.getElementById(room).style.transform = "rotateY(180deg)";
+}
+
+function switchOff(room) {
+    document.getElementById(room).style.transform = "rotateY(0deg)";
+}
